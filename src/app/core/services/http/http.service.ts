24394d19dfc4761b2965ee4f690004fd21d91ad9
Diogo Oliveira <diogorafaellourencooliveira97@gmail.com>
2022-05-23 18:04:16 +0100

    Create Fetch funcs

--- src/app/core/services/http/http.service.ts
+++ src/app/core/services/http/http.service.ts
@@ -1,12 +1,32 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { FetchResponseModel } from 'src/app/shared/models/response/fetch-response.model';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpService {
+  private readonly _serverURL: string;
 
-  constructor(private readonly _httpClient: HttpClient) { }
+  constructor(private readonly _httpClient: HttpClient) {
+    this._serverURL = environment.serverURL;
+  }
 
+  public fetchOneByParams<T>(routeURL: string, httpParams: HttpParams): Observable<FetchResponseModel<T>> {
+    return this._httpClient.get<FetchResponseModel<T>>(`${this._serverURL}/${routeURL}`, { params: httpParams });
+  }
+
+  public fetchOneById<T>(routeURL: string, resourceId: string): Observable<FetchResponseModel<T>> {
+    return this._httpClient.get<FetchResponseModel<T>>(`${this._serverURL}/${routeURL}/${resourceId}`);
+  }
+
+  public fetchBulkByParams<T>(routeURL: string, httpParams: HttpParams): Observable<Array<FetchResponseModel<T>>> {
+    return this._httpClient.get<Array<FetchResponseModel<T>>>(`${this._serverURL}/${routeURL}`, { params: httpParams });
+  }
+
+  public fetchBulkById<T>(routeURL: string, resourceId: string): Observable<Array<FetchResponseModel<T>>> {
+    return this._httpClient.get<Array<FetchResponseModel<T>>>(`${this._serverURL}/${routeURL}/${resourceId}`);
+  }
 }
