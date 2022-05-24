4a9196dc83138e9f1a9560271de70bb4ac4acc4b
Diogo Oliveira <diogorafaellourencooliveira97@gmail.com>
2022-05-24 07:17:48 +0100

    Created util func to icon-service

--- src/app/core/services/icon/icon.service.ts
+++ src/app/core/services/icon/icon.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
 import { IconModel } from 'src/app/shared/models/icon.model';
 
 @Injectable({
@@ -10,4 +11,12 @@ export class IconService {
   constructor() {
     this._icons = new Array<IconModel>();
   }
+
+  /** Utility */
+
+  private _fetchIconDefinition(iconName: string): IconDefinition {
+    return this._icons.find((icon) => icon.name.match(iconName))!.definition;
+  }
+
+  /** Getters */
 }
